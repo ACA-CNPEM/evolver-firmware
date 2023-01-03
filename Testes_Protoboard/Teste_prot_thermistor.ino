b6618e06cb559b19a9d3a3715b4c4108b061bf22
Augusto Vieira <110844153+AugustoVieiraCNPEM@users.noreply.github.com>
2023-01-03 14:28:42 -0300

    Update and rename Teste_prot_thermistor to Teste_prot_thermistor.ino

--- Testes_Protoboard/Teste_prot_thermistor.ino
+++ Testes_Protoboard/Teste_prot_thermistor.ino
@@ -1,2 +1,6 @@
 // O Objetivo deste código é verificar, de maneira simples e superficial, o funcionamento dos pares
 //resistores-termistores das Smart Sleeves.
+
+
+#include <evolver_si.h>
+#include <Tlc5940.h>
