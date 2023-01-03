fdc4dfe3bc49c3b63b4a95d5c3382d6f1cd6ad9a
Augusto Vieira <110844153+AugustoVieiraCNPEM@users.noreply.github.com>
2023-01-03 14:52:35 -0300

    Update Teste_prot_thermistor.ino

--- Testes_Protoboard/Teste_prot_thermistor.ino
+++ Testes_Protoboard/Teste_prot_thermistor.ino
@@ -4,3 +4,26 @@
 
 #include <evolver_si.h>
 #include <Tlc5940.h>
+
+int s0 = 7, s1 = 8, s2 = 9, s3 = 10, SIG_PIN = 0; //define pinos digitais 7-10 do arduino como controles do MUX e analógico 0 como inpt de valores.
+
+void setup(){
+
+SerialUSB.begin(9600);//começa serial USB (comunicação com o monitor serial)
+Serial1.begin(9600);
+pinMode(12, OUTPUT);
+  
+  digitalWrite(12, LOW);
+  pinMode(s0, OUTPUT);
+  pinMode(s1, OUTPUT);
+  pinMode(s2, OUTPUT);
+  pinMode(s3, OUTPUT);
+
+  digitalWrite(s0, LOW);
+  digitalWrite(s1, LOW);
+  digitalWrite(s2, LOW);
+  digitalWrite(s3, LOW);
+
+  analogReadResolution(12);
+
+}
